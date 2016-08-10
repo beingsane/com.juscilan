e854f02f20ff568cacb38742fbe8ccf8029fe222
Juscilan <juscilan@gmail.com>
2016-08-10 11:36:48 -0300

    Fixes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
 import JmHead   from './components/jm-head.vue'
 import JmBody   from './components/jm-body.vue'
 import JmFooter from './components/jm-footer.vue'
+
+Vue.use(VueResource)
 
 new Vue({
   el: 'body',
