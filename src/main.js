4f395e6d5a4d69434086a577265dfa492e8f26c2
Juscilan <juscilan@gmail.com>
2016-08-10 10:24:21 -0300

    css include

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import JmHead from './components/jm-head.vue'
+import JmBody from './components/jm-body.vue'
+
 new Vue({
   el: 'body',
-  components: { App }
+  components: { App,JmHead,JmBody }
 })
