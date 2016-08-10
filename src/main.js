dca0216be59f333efe989320f62c8969a4b4f2d2
Juscilan <juscilan@gmail.com>
2016-08-10 10:46:20 -0300

    Fixes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import JmHead from './components/jm-head.vue'
-import JmBody from './components/jm-body.vue'
+import JmHead   from './components/jm-head.vue'
+import JmBody   from './components/jm-body.vue'
 import JmFooter from './components/jm-footer.vue'
 
 new Vue({
   el: 'body',
-  components: { App,JmHead,JmBody,JmFooter }
+  components: { JmHead,JmBody,JmFooter }
 })
