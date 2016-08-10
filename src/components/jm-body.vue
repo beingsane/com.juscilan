d9b6ca268a67c93c6c834957a7b92f76698457d5
Juscilan <juscilan@gmail.com>
2016-08-10 13:41:43 -0300

    Fixes

--- src/components/jm-body.vue
+++ src/components/jm-body.vue
@@ -2,22 +2,7 @@
       <!-- parte controlada pelo ng -->
       <div id="corpo" ng-app="myapp" onscroll="function(){alert('12');}">
          <div ng-controller="MyController">
-            <span id="forkongithub" class="hide-on-med-and-down"><a href="https://github.com/juscilan">Fork me on GitHub</a></span> 
-            <nav class="white">
-               <div class="nav-wrapper container">
-                  <a id="logo-container" href="/" class="brand-logo"><img src="https://media.licdn.com/mpr/mpr/shrink_100_100/p/7/005/08d/09b/109529d.jpg" class="circle bordabranca" alt="Juscilan Moreto" title="Juscilan Moreto"></a>
-                  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                  <ul class="right hide-on-med-and-down">
-                     <li><a ng-href="#"  ng-click="myData.cv(item, $event)">Curriculum em PDF</a></li>
-                     <li><a ng-href="/##contato" ng-click="myData.novoContato(item, $event)">Contato</a></li>
-                     <li><a ng-href="/##sobre"  ng-click="myData.sobre(item, $event)">Sobre</a></li>
-                  </ul>
-                  <ul class="side-nav" id="mobile-demo">
-                     <li><a ng-href="/##contato" ng-click="myData.novoContato(item, $event)">Contato</a></li>
-                     <li><a ng-href="/##sobre"  ng-click="myData.sobre(item, $event)">Sobre</a></li>
-                  </ul>
-               </div>
-            </nav>
+            
             <h5 class="header center teal-text text-lighten hide-on-med-and-down">{ www.juscilan.com }</h5>
             <div id="index-banner" class="parallax-container">
                <div class="section no-pad-bot">
--- src/main.js
+++ src/main.js
@@ -2,13 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 
-import JmHead   from './components/jm-head.vue'
-import JmBody   from './components/jm-body.vue'
-import JmFooter from './components/jm-footer.vue'
+import JmNav        from './components/jm-1-nav.vue'
+import JmTopBanner  from './components/jm-2-top-banner.vue'
+import JmColluns    from './components/jm-3-colluns.vue'
+import JmMidBanner  from './components/jm-4-mid-banner.vue'
+import JmContact    from './components/jm-5-contact.vue'
+import JmFotBanner  from './components/jm-6-foot-banner.vue'
+import JmFooter     from './components/jm-7-footer.vue'
 
 Vue.use(VueResource)
 
 new Vue({
   el: 'body',
-  components: { JmHead,JmBody,JmFooter }
+  components: { 
+    JmNav
+    ,JmTopBanner 
+    ,JmColluns
+    ,JmMidBanner
+    ,JmContact
+    ,JmFotBanner
+    ,JmFooter        
+}
 })
