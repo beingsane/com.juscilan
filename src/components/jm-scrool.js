58b98a5359b3b4eb20fcfe9f0647d1dcaef9bcf3
Juscilan <juscilan@gmail.com>
2016-08-11 11:07:58 -0300

    Css gulp file

--- src/components/jm-scrool.js
+++ src/components/jm-scrool.js
@@ -1,8 +1,8 @@
 export const scroll = function(element){
-        var $doc = jQuery('html, body');
+        var doc = jQuery('html, body');
         jQuery('.' + element).click(function() {
-            $doc.animate({
-                scrollTop: $( $.attr(this, 'href') ).offset().top
+            doc.animate({
+                scrollTop: jQuery( jQuery.attr(this, 'href') ).offset().top
             }, 1000);
             return false;
         });        
