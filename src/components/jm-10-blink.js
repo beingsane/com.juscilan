f5f816e89d63bed7ac581002c1688cb3ca295671
Juscilan <juscilan@gmail.com>
2016-08-17 14:21:08 -0300

    Blink

---
+++ src/components/jm-10-blink.js
@@ -0,0 +1,7 @@
+export const blink = function(element){
+   var count = 0;
+   do {
+        $('#' + element).fadeOut(500).fadeIn(500);
+        count++;
+   }while(count < 30);    
+}  
--- src/components/jm-2-top-banner.vue
+++ src/components/jm-2-top-banner.vue
@@ -18,15 +18,24 @@
     <h5 class="header center teal-text text-lighten hide-on-med-and-down">"think before, then do."</h5>
     <h5 class="header center teal-text text-lighten hide-on-med-and-up">  
       <i class="fa fa-whatsapp"></i> {{telefone}}
+      <br>
+      <i id="angle-down" class="fa fa-angle-down black-text" style="font-size:48px" > </i>
     </h5>        
 </template>
 
 <script>
+
+import {blink} from './jm-10-blink'
+
 export default {
   data () {
     return {
       telefone:'11 98167-1595'
     }
+  },
+  ready(){
+    blink('angle-down')
+    //jQuery('#angle-down').blink();
   }
 }
 </script>
