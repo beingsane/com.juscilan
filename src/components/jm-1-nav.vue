a27758730c03fc3fec6ed4d7fa1099b26b6bba68
Juscilan <juscilan@gmail.com>
2016-08-24 10:12:35 -0300

    Aling icon materizcss

--- src/components/jm-1-nav.vue
+++ src/components/jm-1-nav.vue
@@ -8,17 +8,17 @@
           <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
           
           <ul class="right hide-on-med-and-down">
-              <li><a href="#nav"      class="scrollSuave">Home</a></li>
+              <li><a href="#nav"      class="scrollSuave">Início</a></li>
               <li><a href="#" @click="AbreModal()">Currículo em PDF</a></li>
               <li><a href="#contato"  class="scrollSuave">Contato</a></li>
               <li><a href="#sobre"    class="scrollSuave">Sobre</a></li>
           </ul>
           
-          <ul class="side-nav" id="mobile-demo">
-              <li><a @click="hide()" class="waves-effect waves-light scrollSuave" href="#nav" ><i class="material-icons left">store</i>Home</a></li>
-              <li><a @click="AbreModal()" class="waves-effect waves-light scrollSuave" href="#nav" ><i class="material-icons left">assignment_ind</i>Currículo PDF</a></li>
-              <li><a @click="hide()" class="waves-effect waves-light scrollSuave" href="#contato" ><i class="material-icons left">email</i>Contato</a></li>
-              <li><a @click="hide()" class="waves-effect waves-light scrollSuave" href="#sobre" ><i class="material-icons left">face</i>Sobre</a></li>
+          <ul class="side-nav" id="mobile-demo" >
+              <li><a @click="hide()"      class="waves-effect waves-light scrollSuave" href="#nav" >    <i class="material-icons left material-icons-align" >store</i>Início</a></li>
+              <li><a @click="AbreModal()" class="waves-effect waves-light scrollSuave" href="#nav" >    <i class="material-icons left material-icons-align" >assignment_ind</i>Currículo PDF</a></li>
+              <li><a @click="hide()"      class="waves-effect waves-light scrollSuave" href="#contato" ><i class="material-icons left material-icons-align" >email</i>Contato</a></li>
+              <li><a @click="hide()"      class="waves-effect waves-light scrollSuave" href="#sobre" >  <i class="material-icons left material-icons-align" >face</i>Sobre</a></li>
           </ul>
         </div>
     </nav>
